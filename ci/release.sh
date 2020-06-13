fa02a8fca5ba01f8e7d51b7b4e933a86743a4ed7
Christopher <christopher.schulz@student.hpi.de>
2020-06-13 16:18:54 +0200

    [FEAT] add Versioning

--- ci/release.sh
+++ ci/release.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-export CEPTA_ROOT=$(dirname "${BASH_SOURCE}")/..
-cd $CEPTA_ROOT
+export CEPMODEMON_ROOT=$(dirname "${BASH_SOURCE}")/..
+cd $CEPMODEMON_ROOT
 
 # Assure all internal tests succeed
-echo "Running tests and checks"
-bazel test :internal --test_output=errors
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+#echo "Running tests and checks"
+#bazel test :internal --test_output=errors
+#rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 # Bump the version
 echo "Incrementing the version"
@@ -18,7 +18,6 @@ source ./ci/print-workspace-status.sh > /dev/null
 
 echo "Preparing for release of ${STABLE_DOCKER_TAG}"
 
-./ci/publish-anubis.sh ${STABLE_DOCKER_TAG}
 
 # Build and push the docker containers (requires docker registry access)
 echo "Building and publishing docker images to docker hub"
