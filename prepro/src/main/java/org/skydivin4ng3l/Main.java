19275a059ac8823abe562b542fb79f5670dcc490
Christopher <christopher.schulz@student.hpi.de>
2020-06-13 16:33:36 +0200

    Bump version: 0.1.0 → 0.1.1

--- prepro/src/main/java/org/skydivin4ng3l/Main.java
+++ prepro/src/main/java/org/skydivin4ng3l/Main.java
@@ -45,7 +45,7 @@ import picocli.CommandLine.Mixin;
 @Command(
 		name = "cepmodemon prepro",
 		mixinStandardHelpOptions = true,
-		version = "0.1.0",
+		version = "0.1.1",
 		description = "PreProcesses the cepta events coming from the Kafka Monitoring queue.")
 public class Main implements Callable<Integer> {
 
